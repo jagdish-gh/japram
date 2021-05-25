cade8beea616ed78e9d1258b4d15da81a9d66c85
Jagdish Choudhary <jagdish.choudhary@mindera.com>
2021-05-25 16:23:13 +0530

    added some music

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import Hugo from './HugoImage.jpg';
+import music from './music.mp3';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  // ;
-  componentDidMount(){
-    const iframe = document.querySelector('iframe');
-    // .contentDocument.body.querySelector('#cdt').style.display = 'none'
-    if(iframe.contentDocument.body){
-      console.log(typeof iframe.contentDocument,iframe.contentDocument.body.querySelector('#cdt'));  
-    }
-    
-  }
-  componentDidUpdate(){
-    const iframe = document.querySelector('iframe');
-    // .contentDocument.body.querySelector('#cdt').style.display = 'none'
-    console.log(typeof iframe.contentDocument);
-  }
+
   render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Hugo} alt={"hugo"}/>
+        <div className="image"><img src={Hugo} alt={"hugo"}/></div>
       </header>
       <section>
-        <iframe width="700" height="360" src="https://w2.countingdownto.com/3625402" frameborder="0"></iframe>
+        <iframe width="700" height="360" src="https://w2.countingdownto.com/3625402" frameborder="0" title="sdffefeff" ></iframe>
       </section>
       <footer>
           <div>boa sorte para suas futuras aventuras! Vejo você por aí.</div>
+          <div> <audio ref="audio_tag" src={music} controls autoPlay loop/></div>
       </footer>
     </div>
   );
