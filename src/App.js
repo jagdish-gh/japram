2a48def1346e968838d47d5ee44f7054d7fe267c
Jagdish Choudhary <jagdish.choudhary@mindera.com>
2021-05-25 11:37:11 +0530

    hugo farewell countdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import Hugo from './HugoImage.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Jay Shree Ram 
+        <img src={Hugo} alt={"hugo"}/>
       </header>
+      <section>
+        <iframe width="700" height="360" src="https://w2.countingdownto.com/3625402" frameborder="0"></iframe>
+      </section>
+      <footer>
+          boa sorte para suas futuras aventuras! Vejo você por aí.
+      </footer>
     </div>
   );
 }
