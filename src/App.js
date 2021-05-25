3bfb7eb84e93d09a826bfd72e1a25f696ad5bdc0
Jagdish Choudhary <jagdish.choudhary@mindera.com>
2021-05-25 13:16:57 +0530

    added favicon

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import Hugo from './HugoImage.jpg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  // ;
+  componentDidMount(){
+    const iframe = document.querySelector('iframe');
+    // .contentDocument.body.querySelector('#cdt').style.display = 'none'
+    if(iframe.contentDocument.body){
+      console.log(typeof iframe.contentDocument,iframe.contentDocument.body.querySelector('#cdt'));  
+    }
+    
+  }
+  componentDidUpdate(){
+    const iframe = document.querySelector('iframe');
+    // .contentDocument.body.querySelector('#cdt').style.display = 'none'
+    console.log(typeof iframe.contentDocument);
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -13,10 +29,11 @@ function App() {
         <iframe width="700" height="360" src="https://w2.countingdownto.com/3625402" frameborder="0"></iframe>
       </section>
       <footer>
-          boa sorte para suas futuras aventuras! Vejo você por aí.
+          <div>boa sorte para suas futuras aventuras! Vejo você por aí.</div>
       </footer>
     </div>
   );
+  }
 }
 
 export default App;
