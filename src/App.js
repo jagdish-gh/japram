0bb029b156319c15c43dd96cdecb80aa6f0ba931
Jagdish Choudhary <jagdish.choudhary@mindera.com>
2021-05-28 17:03:13 +0530

    1

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import { Component } from 'react';
 class App extends Component {
 
   render(){
+    const audio = "https://cdn.sndup.net/32hb/src_music.mp3?token=UzHbrwHG5FzM8feaNsDnVJsAjHidQEGRgQbqGmtpgdg&token_path=%2F32hb%2F&expires=1622202279";
   return (
     <div className="App">
       <header className="App-header">
         <div className="image"><img src={Hugo} alt={"hugo"}/></div>
       </header>
       <section>
-        <iframe width="700" height="360" src="https://w2.countingdownto.com/3625402" frameborder="0" title="sdffefeff" ></iframe>
+        <iframe width="700" height="360" src="https://w2.countingdownto.com/3631319" frameborder="0"></iframe>
       </section>
       <footer>
           <div>boa sorte para suas futuras aventuras! Vejo você por aí.</div>
           <div> 
-            <audio ref="audio_tag" src={"https://cdn.sndup.net/32hb/src_music.mp3?token=ZguwPOoZ1x5kKYCAVXsfzeJ3v_0oK7o3_8FIeCzJPGk&token_path=%2F32hb%2F&expires=1621942947"} controls autoPlay loop/>
+            <audio ref="audio_tag" src={audio} controls autoPlay loop/>
             
           </div>
       </footer>
